0ccae74f86a0514195c1fd0d0c15891ff76e7363
testengcode <testengcode@gmail.com>
2025-06-01 07:59:58 +0200

    Testing

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 try {
-  mongoose.connect("mongodb://localhost:27017/Ecommerce", {
+  mongoose.connect("mongodb://localhost:27017/MERN_Stack_Project_Ecommerce_Hayroo", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
